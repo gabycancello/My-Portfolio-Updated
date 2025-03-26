8fea2803225dbb9a6252b67c51905518ecaa5a8d
Gabriela Cancello <70165237+gabycancello@users.noreply.github.com>
2025-03-26 15:45:01 -0300

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -48,7 +48,7 @@ function showProjects(projects) {
         projectsHTML += `
         <div class="grid-item ${project.category}">
             <div class="box tilt" style="width: 380px; margin: 1rem">
-                <img draggable="false" src="${project.image}" alt="${project.name}" />
+                <img draggable="false" src="${window.location.origin}/${project.image}" alt="${project.name}" />
                 <div class="content">
                     <div class="tag">
                         <h3>${project.name}</h3>
